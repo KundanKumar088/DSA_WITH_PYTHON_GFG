abffc621ef6a741ce1b586b3d5bfec65f5a14a83
Kundan Kumar <190259337+KundanKumar088@users.noreply.github.com>
2026-07-17 14:26:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.py
@@ -0,0 +1,24 @@
+class Solution:
+    def smallestSubWithSum(self, x, arr):
+        # Your code goes here 
+        n=len(arr)
+        min_len = float('inf')
+        curr_sum =0
+        left = 0
+        
+        
+        for right in range(n):
+            curr_sum += arr[right]
+            
+            while curr_sum>x:
+                min_len = min(min_len, right-left+1)
+                curr_sum-=arr[left]
+                left +=1
+                
+        if min_len ==float('inf'):
+            return 0
+        else:
+            return min_len
+        
+        
+        
